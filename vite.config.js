26b4c83f28962f3039d41a3ce82bb699e1854b2e
sirilca <sirilcantony@gmail.com>
2023-12-23 04:32:11 +0530

    vite config to build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', //  To Specify the output directory as "build" or it will be dist 
+  },
 })
+
